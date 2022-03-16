a481006045f2f5010f2e400f545de6427511a7fe
dsolse <davidelezarsegovia@gmail.com>
2022-03-16 17:21:20 -0600

    added usuarios db model

--- models/usuarios.py
+++ models/usuarios.py
@@ -1,10 +1,19 @@
 from db.db import db
 
-class Usuarios(db.Model):
+class Usuarios(db.Model): # type: ignore
 	# Campos
-	id_usuario = db.Column(db.Integer, nullable=False)
+	"""
+	Campos de tabla usuarios:
+		id_user : str
+		nickname : str
+		password : str
+	"""
+	id_user = db.Column(db.Integer, primary_key=True, nullable=False)
+	nickname = db.Column(db.String(50), nullable=False)
+	password = db.Column(db.String(100), nullable=False)
 
-	def __init__(self, usuario_nombre : str, password : str) -> None:
-	    self.usuario_nombre = usuario_nombre
+	def __init__(self, usuario_nombre : str, nickname : str, password : str) -> None:
+		self.usuario_nombre = usuario_nombre
 		self.password = password
+		self.nickname = nickname
 
